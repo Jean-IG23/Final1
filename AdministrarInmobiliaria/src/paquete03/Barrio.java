65b49487afabed8be62cce844a3dd204d36fe12f
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2024-06-05 23:38:34 -0500

    Update Barrio.java

--- AdministrarInmobiliaria/src/paquete03/Barrio.java
+++ AdministrarInmobiliaria/src/paquete03/Barrio.java
@@ -2,34 +2,34 @@ package paquete03;
 
 import java.io.Serializable;
 
-public class Barrio implements Serializable{
+public class Barrio implements Serializable {
     private String nombreBarrio;
     private String referencia;
-
-    public Barrio(){
-
-    }
 
     public Barrio(String nombreBarrio, String referencia) {
         this.nombreBarrio = nombreBarrio;
         this.referencia = referencia;
     }
 
-    public String getNombreBarrio() {
-        return nombreBarrio;
-    }
-
-    public String getReferencia() {
-        return referencia;
-    }
-
-    public void setNombreBarrio(String nombreBarrio) {
-
+    public void establecerNombreBarrio(String nombreBarrio) {
         this.nombreBarrio = nombreBarrio;
     }
 
-    public void setReferencia(String referencia) {
-
+    public void establecerReferencia(String referencia) {
         this.referencia = referencia;
+    }
+
+    public String obtenerNombreBarrio() {
+        return nombreBarrio;
+    }
+
+    public String obtenerReferencia() {
+        return referencia;
+    }
+    
+     @Override
+    public String toString() {
+        return String.format("%s - %s", 
+                obtenerNombreBarrio(), obtenerReferencia());
     }
 }
