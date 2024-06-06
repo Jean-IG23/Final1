f8309579bd664b12604169e12d0fb21272515e5b
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2024-06-05 23:39:16 -0500

    Update Ciudad.java

--- AdministrarInmobiliaria/src/paquete04/Ciudad.java
+++ AdministrarInmobiliaria/src/paquete04/Ciudad.java
@@ -2,35 +2,34 @@ package paquete04;
 
 import java.io.Serializable;
 
-public class Ciudad implements Serializable{
+public class Ciudad implements Serializable {
     private String nombreCiudad;
     private String nombreProvincia;
 
-    public Ciudad(){
-
+    public Ciudad(String nombreCiudad, String nombreProvincia) {
+       this.nombreCiudad = nombreCiudad;
+       this.nombreProvincia = nombreProvincia;
     }
 
-    public Ciudad(String nombreCiudad, String nombreProvincia) {
+    public void establecerNombreCiudad(String nombreCiudad) {
         this.nombreCiudad = nombreCiudad;
+    }
+
+    public void establecerNombreProvincia(String nombreProvincia) {
         this.nombreProvincia = nombreProvincia;
     }
 
-    public String getNombreCiudad() {
-
+    public String obtenerNombreCiudad() {
         return nombreCiudad;
     }
 
-    public String getNombreProvincia() {
+    public String obtenerNombreProvincia() {
         return nombreProvincia;
     }
-
-    public void setNombreCiudad(String nombreCiudad) {
-
-        this.nombreCiudad = nombreCiudad;
-    }
-
-    public void setNombreProvincia(String nombreProvincia) {
-
-        this.nombreProvincia = nombreProvincia;
+    
+     @Override
+    public String toString() {
+        return String.format("%s - %s", 
+                obtenerNombreCiudad(), obtenerNombreProvincia());
     }
 }
